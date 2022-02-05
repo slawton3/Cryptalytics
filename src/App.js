baf65512fea590e51ded66ed2618e4a08976ddc9
slawton <seanlawton3@gmai.com>
2022-02-05 21:48:06 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+import { Navbar, Exchanges, CryptoDetails, Homepage, Cryptocurrencies, News } from './components';
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        <div className="app">
+            <div className='navbar'>
+                <Navbar />
+            </div>
+            <div className='main'>
+                <Layout>
+                    <div className='routes'>
+                        <Routes>
+                            <Route exact path='/' element={<Homepage />} />
+                            <Route exact path='/exchanges' element={<Exchanges />} />
+                            <Route exact path='/cryptocurrencies' element={<Cryptocurrencies />} />
+                            <Route exact path='/crypto/:coinId' element={<CryptoDetails />} />
+                            <Route exact path='/news' element={<News />} />
+                        </Routes>
+                    </div>
+                </Layout>
+            </div>
+            <div className='footer'>
+                
+            </div>
+        </div>
+    )
+};
 
 export default App;
